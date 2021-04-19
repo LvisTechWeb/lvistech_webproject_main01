b73db3b6e8553f1dc90946ebccdc52d8e8489a3f
LvisTechWeb <79597280+LvisTechWeb@users.noreply.github.com>
2021-04-19 19:28:20 +0530

    font changed

--- gatsby-config.js
+++ gatsby-config.js
@@ -48,7 +48,7 @@ module.exports = {
     },
 
     {
-      resolve: `gatsby-plugin-prefetch-google-fonts`,
+      resolve: `gatsby-plugin-preload-fonts`,
       options: {
         fonts: [
           {
